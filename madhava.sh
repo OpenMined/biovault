e9b868bcf600bc134543558b8ba9bcdf665e4652
Madhava Jay <me@madhavajay.com>
2025-08-22 13:49:27 +1000

    Added oca2 nextflow test

---
+++ madhava.sh
@@ -0,0 +1,3 @@
+nextflow run oca2.nf \
+    --ref /Users/madhavajay/dev/sequencing.com/data/nextflow_sarek/results/preprocessing/recalibrated/MadhavaJay-SQA28U66-30x-WGS-Sequencing_com/Homo_sapiens_assembly38.fasta \
+    --cram /Users/madhavajay/dev/sequencing.com/data/nextflow_sarek/results/preprocessing/recalibrated/MadhavaJay-SQA28U66-30x-WGS-Sequencing_com/MadhavaJay-SQA28U66-30x-WGS-Sequencing_com.recal.cram
